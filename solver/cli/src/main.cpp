dc5010e6f35afc5afbfef44b45d6261a9675903c
Anders Wind <awia00@gmail.com>
2019-09-04 00:46:50 +0200

    more const correctness

--- solver/cli/src/main.cpp
+++ solver/cli/src/main.cpp
@@ -1,24 +1,22 @@
 #include <iostream>
 #include <np_solver/dataset_repository.hpp>
 #include <np_solver/graphs/di_graph.hpp>
+#include <np_solver/graphs/graph_iterator.hpp>
 #include <np_solver/graphs/u_graph.hpp>
 #include <np_solver/isomorphism.hpp>
 #include <np_solver/permutation_runner.hpp>
 #include <np_solver/solvers/max_independent_set.hpp>
-#include <np_solver/graphs/graph_iterator.hpp>
 
 namespace npim
 {
-
 void run()
 {
-    constexpr int v = 6;
+    constexpr int v = 5;
     auto max_ind_set_solver = std::make_shared<solvers::MaxIndependentSet<graphs::UGraph<v>>>();
     auto iso_filter = std::make_shared<filters::IsomorphFilter<graphs::UGraph<v>>>();
     auto perm_runner = PermutationRunner<graphs::UGraph<v>>({ iso_filter }, { max_ind_set_solver });
     perm_runner.solve();
 }
-
 } // namespace npim
 
 int main()
